03382fdaa82c8eaf4e807199365b8872692617a8
hyein89 <bihontai@gmail.com>
2026-09-05 14:33:36 +0700

    Update and rename konlolads.js to xxlads.js

--- xxlads.js
+++ xxlads.js
@@ -1,3 +1,4 @@
+// Tulis ini di dalam file JS di GitHub
 var scriptBaru = document.createElement('script');
 scriptBaru.src = "https://dozenfourteen.com/a7/0c/53/a70c53ec64dade158b084af93e2acf60.js";
-document.body.appendChild(scriptBaru);
+document.head.appendChild(scriptBaru); // <-- Ubah ke document.head
