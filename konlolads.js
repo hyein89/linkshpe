c6b8884fd002ca7142ac883cd702e53c18b5c16c
hyein89 <bihontai@gmail.com>
2026-09-05 14:29:39 +0700

    Update and rename adsterakonpo.js to konlolads.js

---
+++ konlolads.js
@@ -0,0 +1,3 @@
+var scriptBaru = document.createElement('script');
+scriptBaru.src = "https://dozenfourteen.com/a7/0c/53/a70c53ec64dade158b084af93e2acf60.js";
+document.body.appendChild(scriptBaru);
